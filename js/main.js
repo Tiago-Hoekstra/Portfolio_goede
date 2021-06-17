d91dfe0547c08a79bb224fdef2391b1bc84398cb
Tiago Hoekstra <302544294@student.rocmondriaan.nl>
2021-06-17 11:00:03 +0200

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
    
     Initial commit
    
     Changes to be committed:
    	new file:   Vakanties/cuba.html
    	new file:   Vakanties/nederland.html
    	new file:   Vakanties/praag.html
    	new file:   Vakanties/spanje.html
    	new file:   Vakanties/vakanties.html
    	new file:   css/index.css
    	new file:   css/navbar.css
    	new file:   index.html
    	new file:   js/main.js
    	new file:   js/rekentool.js
    	new file:   portfolio.html
    	new file:   rekentool.html

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const greenButton = document.querySelector(".green-mode");
+const redButton = document.querySelector(".red-mode");
+const blueButton = document.querySelector(".blue-mode");
+const body = document.body;
+
+greenButton.addEventListener("click", () => {
+	if(body.classList.contains("blue-mode") || body.classList.contains("red-mode")) {
+		body.className = "";
+	}
+	body.classList.toggle("green-mode");
+});
+
+redButton.addEventListener("click", () => {
+	if(body.classList.contains("blue-mode") || body.classList.contains("green-mode")) {
+		body.className = "";
+	}
+	body.classList.toggle("red-mode");
+});
+
+blueButton.addEventListener("click", () => {
+	if(body.classList.contains("green-mode") || body.classList.contains("red-mode")) {
+		body.className = "";
+	}
+	body.classList.toggle("blue-mode");
+});
+
