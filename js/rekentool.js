6ef004fae6394c5a859596b694418e0c1d0c7461
Tiago Hoekstra <302544294@student.rocmondriaan.nl>
2021-06-26 13:10:16 +0200

    BKE

--- js/rekentool.js
+++ js/rekentool.js
@@ -1,20 +1,21 @@
 console.log("JS file loaded")
 
-const inputFieldDistance = document.querySelector(".distance")
-const inputFieldFuelUsage = document.querySelector(".fuel-usage")
-const buttonCalculate = document.querySelector(".calculate")
-const inputFieldFuelPrice = document.querySelector(".fuel-price")
-const outputResult = document.querySelector(".result")
+const inputFieldDistance = document.querySelector(".distance");
+const inputFieldFuelUsage = document.querySelector(".fuel-usage");
+const buttonCalculate = document.querySelector(".calculate");
+const inputFieldFuelPrice = document.querySelector(".fuel-price");
+const outputResult = document.querySelector(".result");
 
 if (buttonCalculate){
     buttonCalculate.addEventListener("click", function() {
-        outputResult.innerHTML = calculate()
-    })
+        outputResult.innerHTML = calculate();
+    });
 }
 
 
 function calculate(){
-    let result = inputFieldDistance.value / inputFieldFuelUsage.value * inputFieldFuelPrice.value
-    return result
+    let result = (inputFieldDistance.value / inputFieldFuelUsage.value) * inputFieldFuelPrice.value;
+    outputResult.textContent = result;
+    console.log(result);
 }
 
